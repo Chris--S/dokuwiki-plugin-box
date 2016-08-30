cc863027d87367b282fdef2d791786a3c1aaa08d
Gerry Weißbach <gerry.w@gammaproduction.de>
2016-08-30 08:03:51 +0200

    Update action.php

--- action.php
+++ action.php
@@ -13,7 +13,7 @@ if(!defined('DOKU_INC')) die();
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'action.php');
 
-class action_plugin_box extends DokuWiki_Action_Plugin {
+class action_plugin_box2 extends DokuWiki_Action_Plugin {
 
 	function register(Doku_Event_Handler $controller) {
 		$controller->register_hook('ACTION_SHOW_REDIRECT', 'BEFORE', $this, 'act_box_redirect_execute');
